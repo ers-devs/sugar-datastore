3288015f79b5b19d35d00e3549f269e2719dfbd1
Christophe Gueret <christophe.gueret@gmail.com>
2014-07-03 16:47:10 +0200

    Fixed the metadastore

--- src/carquinyol/metadatastore.py
+++ src/carquinyol/metadatastore.py
@@ -1,10 +1,8 @@
-import os
 import logging
 import dbus
 
 from ers import ERS
 from carquinyol import layoutmanager
-from carquinyol import metadatareader
 
 MAX_SIZE = 256
 _INTERNAL_KEYS = ['checksum']
@@ -16,32 +14,33 @@ class MetadataStore(object):
         Constructor
         '''
         # Create an instance of ERS
-        self._ers = ERS()        
-
+        self._ers = ERS()
     
     def store(self, uid, metadata):
         '''
-        Store a new entry to the journal
+        Create or update an entry to the journal
         '''
+        logging.warn("Add {}".format(uid))
+        logging.warn("Add {}".format(metadata))
+        
         # Name of the entry
         entity_name = layoutmanager.get_instance().get_entity_name(uid)
 
-        # If already existing, erase the previous version
-        if self._ers.contains_entity(entity_name):
-            self._ers.delete_entity(entity_name)
+        # Get or create the entity
+        entity = None
+        if self._ers.entity_exist(entity_name):
+            entity = self._ers.get_entity(entity_name)
+        else:
+            entity = self._ers.create_entity(entity_name)
     
         # Update the description of the entity
-        entity = self._ers.create_entity(entity_name)
-        metadata['uid'] = uid
+        metadata['uid'] = str(uid)
         for key, value in metadata.items():
-            #if isinstance(value, unicode):
-            #    value = value.encode('utf-8')
-            #elif not isinstance(value, basestring):
-            #    value = str(value)
-            if type(value) == dbus.Int32 or type(value) == dbus.String:
-                entity.add_property(key, value)
-            else:
-                logging.warn("Skipped " + key + "(" + str(type(value)) + ")")
+            entity.set_property_value(key, value, private=True)
+            #if type(value) == dbus.Int32 or type(value) == dbus.String or (str(type(value)) in ["<type 'str'>", "<type 'int'>"]):
+            #    entity.set_property_value(key, value, private=True)
+            #else:
+            #    logging.warn("Skipped " + key + "(" + str(type(value)) + ")")
     
         # Persist the result
         self._ers.persist_entity(entity)
@@ -50,15 +49,14 @@ class MetadataStore(object):
         '''
         Retrieve some properties of a journal entry
         '''
-        # Name of the entry
+        # Name of the entry        
         entity_name = layoutmanager.get_instance().get_entity_name(uid)
-        
         # Get all the (accessible) documents describing that identifier
         entity = self._ers.get_entity(entity_name)
         
         # Get all the properties
         description = entity.get_properties()
-        
+                
         # TODO if properties != None filter the output
         return description
 
@@ -91,27 +89,19 @@ class MetadataStore(object):
     def set_property(self, uid, key, value):
         '''
         Set a single property
-        '''
+        '''        
         # Name of the entry
         entity_name = layoutmanager.get_instance().get_entity_name(uid)
 
         # Get all the (accessible) documents describing that identifier
         entity = self._ers.get_entity(entity_name)
 
-        # Get all the properties
-        description = entity.get_properties()
-        
-        # Remove previous values
-        if key in description:
-            for v in description[key]:
-                entity.delete_property(key, v)
-        
         # Set new value
         if isinstance(value, unicode):
             value = value.encode('utf-8')
         elif not isinstance(value, basestring):
             value = str(value)
-        entity.add_property(key, value)
+        entity.set_property_value(key, value, private=True)
         
         # Persist
         self._ers.persist_entity(entity)
